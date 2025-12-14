1929865467351d4588b6798be9eaf107bb40d286
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-14 08:56:15 +0100

    Create CreateModelSoftOnLatentModelsOne.h

---
+++ CreateModelSoftOnLatentModelsOne.h
@@ -0,0 +1,4 @@
+//we can use six parameters for several rows of the message model between hd and cpu and add UI/UX models parameters to crack. But to crrack too,
+//we need to use ciphers oon preprocessing datas which would fill the messages model or models. Then, if we use clusters alhebra, we can omit
+//first messages of the cpu to hd. So the hd is secured but not fullfilled. The cryptographic singals become easier in latent space
+//but not topologies. So we have to send an hidden network by collaborative messages between servers and hosts.
