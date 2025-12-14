86322ff54867c7c0ebea859d28482ffaac7331d7
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-14 09:19:15 +0100

    Create StrangeBoxCiphersToCommunicateOnCrash.h

---
+++ StrangeBoxCiphersToCommunicateOnCrash.h
@@ -0,0 +1,12 @@
+//We can have several cash during exchanges with no timers. The timed behaviour of this weak box could help communicating.
+//Again, in order to not fullfill the ud, we have to overcome the digits ressponsible to random messages on hd internal behaviour.
+//This complex chaotic system would be also solved by strong box another hd. So this best to add algorithms to all thoe parts communications.
+//We dont deal with devices cyber security here. But if you want to do that, you should keep track even in those weak boxes. Plus,
+clusters must be filled with real numbers due to even weak ciphers. The latent space is now unknown which is the beahviour of the server. That said,
+//the server should not overcome real digits if its recurrent task is to manage hosts by sniffing.
+
+class SniffersAlgorithmNotWeakAlgorithmOnDevicesHostAnotherAlgorithmCipher
+{
+
+
+}
